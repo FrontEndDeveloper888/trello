91da2d971fdd839c9bf0c04d5e85ab7b236c493c
FrontEndDeveloper888 <frontenddeveloper2008@gmail.com>
2023-08-04 02:34:48 +0500

    Finish lists

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,13 +1,13 @@
 import {
     Stack,
-    List,
-    ListItem,
-    ListItemText,
     Box,
     Typography,
+    Button,
+    Modal,
+    TextField,
 } from "@mui/material";
 import * as React from "react";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { BoardsContext } from "../../context";
 import { StyledList, styles } from "./styles";
 import { TaskDialog } from "../TaskDialog";
@@ -20,24 +20,74 @@ export const Board = (props) => {
     const [open, setOpen] = React.useState(false);
     const [listIndex, setListIndex] = React.useState(null);
     const [taskIndex, setTaskIndex] = React.useState(null);
+    const [newListTitle, setNewListTitle] = useState("");
+    const [openTaskModal, setOpenTaskModal] = React.useState(false);
+    const [newTaskTitle, setNewTaskTitle] = useState("");
+    const [editListTitle, setEditListTitle] = useState("");
+    const [openTaskEditModal, setOpenTaskEditModal] = React.useState(false);
 
-
-
-    const handleClose = (value) => {
+    const handleClose = () => {
         setOpen(false);
         setListIndex(null);
         setTaskIndex(null);
+        setNewListTitle("");
     };
+
+    const handleAddTaskModal = (listIndex) => {
+        setOpenTaskModal(true);
+        setListIndex(listIndex);
+    };
+
     const handleAddTask = (listIndex) => {
+        if (newTaskTitle.trim() === "") {
+            return;
+        }
+
         const updatedBoards = [...boards];
-        const newTask = { title: `New Task ${Date.now()}`, description: "" };
+        const newTask = { title: newTaskTitle, description: "" };
         updatedBoards[activeBoardIndex].lists[listIndex].tasks.push(newTask);
         setBoards(updatedBoards);
+        setOpenTaskModal(false);
+        setNewTaskTitle("");
+    };
+
+    const handleAddList = () => {
+        if (newListTitle.trim() === "") {
+            return;
+        }
+
+        const updatedBoards = [...boards];
+        const newList = { title: newListTitle, tasks: [] };
+        updatedBoards[activeBoardIndex].lists.push(newList);
+        setBoards(updatedBoards);
+        handleClose();
+    };
+
+    const handleDeleteList = (listIndex) => {
+        const updatedBoards = [...boards];
+        updatedBoards[activeBoardIndex].lists.splice(listIndex, 1);
+        setBoards(updatedBoards);
+    };
+
+    const handleEditListModal = (listIndex) => {
+        setOpenTaskEditModal(true);
+        setListIndex(listIndex);
+        setEditListTitle(boards[activeBoardIndex].lists[listIndex].title);
+    };
+
+    const handleEditList = () => {
+        if (editListTitle.trim() === "") {
+            return;
+        }
+        const updatedBoards = [...boards];
+        updatedBoards[activeBoardIndex].lists[listIndex].title = editListTitle;
+        setBoards(updatedBoards);
+        setOpenTaskEditModal(false);
     };
 
 
     return (
-        <Stack direction={"row"} spacing={2}>
+        <Stack direction="row" spacing={2}>
             <TaskDialog
                 values={{ listIndex, taskIndex }}
                 open={open}
@@ -45,17 +95,118 @@ export const Board = (props) => {
             />
             {boards[activeBoardIndex].lists.map((list, listIdx) => (
                 <StyledList>
-                    <Typography sx={{ mb: 2 }}>{list.title}</Typography>
+                    <Typography onClick={() => handleEditListModal(listIdx)} sx={{ mb: 2 ,cursor:"pointer"}}>{list.title}</Typography>
                     <Box>
                         {list.tasks.map((task, taskIdx) => (
-                            <Box
-                                className={"list"}>
-                                    <Typography>{task.title}</Typography>
+                            <Box className="list" key={taskIdx}>
+                                <Typography>{task.title}</Typography>
                             </Box>
                         ))}
                     </Box>
+                    <Button
+                        sx={styles.buttonTask}
+                        onClick={() => handleAddTaskModal(listIdx)}
+                    >
+                        + Yangi Task
+                    </Button>
+                    <Button
+                        sx={styles.buttonTask}
+                        onClick={() => handleDeleteList(listIdx)}>
+                        - List
+                    </Button>
                 </StyledList>
             ))}
+            <Button sx={styles.button} onClick={() => setOpen(true)}>
+                + Yangi List
+            </Button>
+
+            <Modal open={open} onClose={handleClose}>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "20%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        bg: "white",
+                        p: 2,
+                        m: 1,
+                        outline: "none",
+                        borderRadius: 3,
+                        backgroundColor: "rgba(255, 255, 255, 0.7)",
+                    }}
+                >
+                    <TextField
+                        color="primary"
+                        value={newListTitle}
+                        onChange={(e) => setNewListTitle(e.target.value)}
+                        placeholder="List title..."
+                        size="small"
+                    />
+                    <Button onClick={handleAddList} variant="contained" sx={{ p: 1, ml: 1 }}>
+                        Save
+                    </Button>
+                </Box>
+            </Modal>
+
+            {/* Task qo'shish uchun modal */}
+            <Modal open={openTaskModal} onClose={() => setOpenTaskModal(false)}>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "20%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        bg: "white",
+                        p: 2,
+                        m: 1,
+                        outline: "none",
+                        borderRadius: 3,
+                        backgroundColor: "rgba(255, 255, 255, 0.7)",
+                    }}
+                >
+                    <TextField
+                        color="primary"
+                        value={newTaskTitle}
+                        onChange={(e) => setNewTaskTitle(e.target.value)}
+                        placeholder="Task title..."
+                        size="small"
+                    />
+                    <Button
+                        onClick={() => handleAddTask(listIndex)}
+                        variant="contained"
+                        sx={{ p: 1, ml: 1 }}
+                    >
+                        Save Task
+                    </Button>
+                </Box>
+            </Modal>
+            <Modal  open={openTaskEditModal} onClose={() => setOpenTaskEditModal(false)}>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "20%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        bg: "white",
+                        p: 2,
+                        m: 1,
+                        outline: "none",
+                        borderRadius: 3,
+                        backgroundColor: "rgba(255, 255, 255, 0.7)",
+                    }}
+                >
+                    <TextField
+                        color="primary"
+                        value={editListTitle}
+                        onChange={(e) => setEditListTitle(e.target.value)}
+                        placeholder="List title..."
+                        size="small"
+                    />
+                    <Button onClick={handleEditList} variant="contained" sx={{ p: 1, ml: 1 }}>
+                        Save List Title
+                    </Button>
+                </Box>
+            </Modal>
         </Stack>
     );
 };
--- src/components/Board/styles.js
+++ src/components/Board/styles.js
@@ -1,9 +1,17 @@
 import { styled, Box } from "@mui/material";
-
 export const styles = {
   board: { background: "#101204", p: 2, width: 240, borderRadius: 3, color: "#a2acb6" },
-  button: { width: 240, textTransform: "none", color: "white", borderRadius: 3, p: 1, mb: 1, backgroundColor: "rgba(0, 0, 0, 0.3)", backdropFilter: "blur(1px)" },
-  add: { height: 5 },
+  button: { width: 240, height: 50, textTransform: "none", color: "white", borderRadius: 3, p: 1, mb: 1, backgroundColor: "rgba(0, 0, 0, 0.3)", backdropFilter: "blur(1px)" },
+  buttonTask: {
+    textTransform: "none",
+    color: "white",
+    borderRadius: 2,
+    padding:1,
+    marginBottom: 1,
+    backgroundColor: "rgba(0,0,0,0.3)",
+    backdropFilter: "blur(1px)",
+    width: "100%",
+  },
   color: { background: "#2c3033" },
   main: { minHeight: 100, flexGrow: 1, p: 3 },
   task: { width: '100%', textTransform: "none", color: "white", borderRadius: 3, p: 1, mb: 1, backgroundColor: "rgba(0, 0, 0, 0.3)", backdropFilter: "blur(1px)" }
